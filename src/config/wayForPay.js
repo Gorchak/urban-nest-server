335b9511e7e3e549d642d42f84b5c702a040dc4e
petrohorchak <petro@creaition.io>
2026-08-05 13:18:59 +0300

    fix payment

--- src/config/wayForPay.js
+++ src/config/wayForPay.js
@@ -4,7 +4,8 @@ const getWayForPayConfig = () => ({
   merchantSecretKey: process.env.WAYFORPAY_MERCHANT_SECRET_KEY || '',
   merchantDomainName: process.env.WAYFORPAY_MERCHANT_DOMAIN || '',
   paymentUrl: process.env.WAYFORPAY_PAYMENT_URL || 'https://secure.wayforpay.com/pay',
-  serviceUrl: process.env.WAYFORPAY_SERVICE_URL || '',
+  serviceUrl: process.env.WAYFORPAY_SERVICE_URL
+    || 'https://api.uliastore.com.ua/api/sales/wayforpay/callback',
   returnUrl: process.env.WAYFORPAY_RETURN_URL || '',
 });
 
